a4633cc398d3f904c9af2a3d3712327fccbfd687
Maher Gamal <mahergamal@gmail.com>
2012-03-05 06:46:50 +0200

    Moved the current BinarySearch tests to a mother class.

--- codekata2/src/test/java/mahergamal/codekata2/RecursiveBinarySearchTests.java
+++ codekata2/src/test/java/mahergamal/codekata2/RecursiveBinarySearchTests.java
@@ -1,73 +1,10 @@
 package mahergamal.codekata2;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
-public class RecursiveBinarySearchTests {
+public class RecursiveBinarySearchTests extends AbstractBinarySearchTests {
     
-    BinarySearch search = new RecursiveBinarySearch();
-    int[] numberList = new int[] { 0, 1, 3, 4, 6, 10, 23, 29, 44, 69, 102, 306, 545 };
-    int infoundableTarget = 20;
-    
-    @Test
-    public void codeKataAssertions() {
-        assertEquals(-1, search.find(new int[] {}, 3));
-        assertEquals(-1, search.find(new int[] { 1 }, 3));
-        assertEquals(0, search.find(new int[] { 1 }, 1));
-        
-        assertEquals(0, search.find(new int[] { 1, 3, 5 }, 1));
-        assertEquals(1, search.find(new int[] { 1, 3, 5 }, 3));
-        assertEquals(2, search.find(new int[] { 1, 3, 5 }, 5));
-        assertEquals(-1, search.find(new int[] { 1, 3, 5 }, 0));
-        assertEquals(-1, search.find(new int[] { 1, 3, 5 }, 2));
-        assertEquals(-1, search.find(new int[] { 1, 3, 5 }, 4));
-        assertEquals(-1, search.find(new int[] { 1, 3, 5 }, 6));
-        
-        assertEquals(0, search.find(new int[] { 1, 3, 5, 7 }, 1));
-        assertEquals(1, search.find(new int[] { 1, 3, 5, 7 }, 3));
-        assertEquals(2, search.find(new int[] { 1, 3, 5, 7 }, 5));
-        assertEquals(3, search.find(new int[] { 1, 3, 5, 7 }, 7));
-        assertEquals(-1, search.find(new int[] { 1, 3, 5, 7 }, 0));
-        assertEquals(-1, search.find(new int[] { 1, 3, 5, 7 }, 2));
-        assertEquals(-1, search.find(new int[] { 1, 3, 5, 7 }, 4));
-        assertEquals(-1, search.find(new int[] { 1, 3, 5, 7 }, 6));
-        assertEquals(-1, search.find(new int[] { 1, 3, 5, 7 }, 8));
+    @Override
+    public void setup() {
+        search = new RecursiveBinarySearch();
     }
     
-    @Test
-    public void shouldFindMiddleTarget() {
-        int index = search.find(numberList, 23);
-        assertEquals(6, index);
-    }
-    
-    @Test
-    public void shouldFindFirstTarget() {
-        int index = search.find(numberList, 0);
-        assertEquals(0, index);
-    }
-    
-    @Test
-    public void shouldFindLastTarget() {
-        int index = search.find(numberList, 545);
-        assertEquals(12, index);
-    }
-    
-    @Test
-    public void shouldFindTargetInUpperSlice() {
-        int index = search.find(numberList, 69);
-        assertEquals(9, index);
-    }
-    
-    @Test
-    public void shouldFindTargetInLowerSlice() {
-        int index = search.find(numberList, 3);
-        assertEquals(2, index);
-    }
-    
-    @Test
-    public void shouldReturnNegativeOneIfTargetDoesntExist() {
-        int index = search.find(numberList, infoundableTarget);
-        assertEquals(-1, index);
-    }
 }
